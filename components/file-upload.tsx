12927c3ce816483a672a01c5f56b8e0118913f8a
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-08-12 23:55:37 +0530

    Completed Message Attachment Functionality
    TODO: Emoji Bar

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import "@uploadthing/react/styles.css";
 import { UploadButton, UploadDropzone } from "@/lib/uploadthing";
 import Image from "next/image";
-import { XIcon } from "lucide-react";
+import { FileIcon, XIcon } from "lucide-react";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
@@ -26,6 +26,29 @@ const FileUpload: React.FC<FileUploadProps> = ({
         <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <XIcon className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
           type="button"
         >
           <XIcon className="h-4 w-4" />

dfa0e10e97f6fb0115a32dbe7142db0b6b6ab491
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2024-08-12 20:44:17 +0530

    Chat input completed
    TODO: Messages API

--- app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,9 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import { serverHooks } from "next/dist/server/app-render/entry-base";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -32,9 +34,25 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 
   if (!channel || !member) return redirect("/");
 
-  return <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-    <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
-  </div>;
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
